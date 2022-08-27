63109d2dbb87d5e9ba49f32a20fb810bf8ae3c5e
ARSAKAEV <arsakaev999555@gmail.com>
2022-08-27 17:22:50 +0300

    work

---
+++ src/Detective.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function Detective () {
+    const [sherlock, setSherlock] = useState('психопат') 
+    const handleClick = () => {
+        setSherlock('высокоактивный социопат')
+    }
+
+    return (
+        <>
+        <p>
+            Шерлок - {sherlock}
+        </p>
+        <div>
+            <button onClick={handleClick}>узнать правду</button>
+        </div>
+        </>
+    )
+};
+export default Detective;
